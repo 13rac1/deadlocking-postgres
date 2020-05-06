4759678206d54c9d12d246ef9b6caabfbbca0b46
Brad Erickson <bderickson@gmail.com>
2020-05-05 22:58:12 -0700

    feat: Reliable database level deadlock created
    ERROR:  deadlock detected
    DETAIL:  Process 62 waits for AccessExclusiveLock on relation 16386 of database 12138; blocked by process 64.
             Process 64 waits for ShareLock on transaction 684; blocked by process 62.
             Process 62: ALTER TABLE users ADD COLUMN counter TEXT;
             Process 64: INSERT INTO users(first_name, last_name, email) VALUES ($1, $2, $3) RETURNING "id";

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"syscall"
 	"time"
@@ -16,17 +17,23 @@ import (
 )
 
 const (
-	pgImage         = "postgres:9.4-alpine"
-	pgContainerName = "postgres"
-	pgContainerPort = "5432"
-	pgHostPort      = "5432"
-	pgPassword      = "postgres"
+	pgImage = "postgres:9.4-alpine"
 )
 
 var (
 	cli            *client.Client
 	defaultTimeout = 10 * time.Second
 )
+
+type pgStatActivity struct {
+	Pid             int
+	UserName        string `db:"usename"`
+	ApplicationName string `db:"application_name"`
+	ClientAddress   string `db:"client_addr"`
+	Waiting         bool
+	State           string
+	Query           string
+}
 
 func main() {
 	ctx := context.Background()
@@ -50,17 +57,140 @@ func main() {
 	defer docker.removeContainer(ctx, pgContainer.ID)
 	go docker.printLogs(ctx, pgContainer.ID)
 
-	err = waitForPostgresReady(ctx, addr)
+	db, err := waitForPostgresReady(ctx, addr)
 	if err != nil {
 		fmt.Println("failed waiting on Postgres")
 		panic(err)
 	}
+
+	stopStatus := make(chan bool)
+	go printConnectionStats(ctx, db, stopStatus)
+	setupSchemaUsers(ctx, db)
+
+	test1DB, err := sqlx.Connect("postgres", "postgres://test:test@localhost/postgres?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	tx0, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	tx1, err := test1DB.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = tx1.ExecContext(ctx,
+		`INSERT INTO users(first_name, last_name, email)
+		VALUES ($1, $2, $3) RETURNING "id";`,
+		"test1", "test1", "test1@example.com")
+	if err != nil {
+		panic("tx1-1: " + err.Error())
+	}
+
+	go func() {
+		_, err = tx0.ExecContext(ctx,
+			`INSERT INTO users(first_name, last_name, email)
+			VALUES ($1, $2, $3) RETURNING "id";`,
+			"test2", "test2", "test2@example.com")
+		if err != nil {
+			log.Print("tx0-1: " + err.Error())
+			return
+		}
+		// Conflicts with INSERT of test1@example.com
+		_, err = tx0.ExecContext(ctx,
+			`ALTER TABLE users ADD COLUMN counter TEXT;`)
+		if err != nil {
+			log.Print("tx0-2: " + err.Error())
+		}
+	}()
+	time.Sleep(100 * time.Millisecond)
+	// Conflicts with INSERT of test2@example.com
+	_, err = tx1.ExecContext(ctx,
+		`INSERT INTO users(first_name, last_name, email)
+		VALUES ($1, $2, $3) RETURNING "id";`,
+		"test3", "test3", "test2@example.com")
+	if err != nil {
+		panic("tx1-2: " + err.Error())
+	}
+
+	err = tx1.Commit()
+	if err != nil {
+		panic(err)
+	}
+
+	err = test1DB.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	// Disabled, because this a failed transaction.
+	// err = tx0.Commit()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	err = db.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	// Stop connection status display
+	stopStatus <- true
 }
 
-func waitForPostgresReady(ctx context.Context, addr string) error {
+func printConnectionStats(ctx context.Context, db *sqlx.DB, stop chan bool) {
+	for true {
+		select {
+		case <-stop:
+			fmt.Println("Stopping status loop")
+			return
+		default:
+			var statActivity []pgStatActivity
+			err := db.SelectContext(ctx, &statActivity, "select pid, usename, application_name, client_addr, waiting, state, query from pg_stat_activity;")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("\n%#v count:%d\n", statActivity, len(statActivity))
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func setupSchemaUsers(ctx context.Context, db *sqlx.DB) {
+	_, err := db.Exec(`
+	CREATE TABLE users (
+		id SERIAL PRIMARY KEY,
+		first_name TEXT,
+		last_name TEXT,
+		email TEXT,
+		UNIQUE (email)
+	)`)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`CREATE USER test WITH ENCRYPTED PASSWORD 'test';`)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO test;`)
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec(`GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO test;`)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func waitForPostgresReady(ctx context.Context, addr string) (*sqlx.DB, error) {
 	err := waitForPort(addr)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	fmt.Println("connecting DB")
@@ -68,14 +198,14 @@ func waitForPostgresReady(ctx context.Context, addr string) error {
 	// manually ping the DB until ready.
 	db, err := sqlx.Open("postgres", "postgres://postgres:postgres@localhost/postgres?sslmode=disable")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = pingDB(ctx, db)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return db, nil
 }
 
 func waitForPort(addr string) error {
@@ -103,12 +233,12 @@ func pingDB(ctx context.Context, db *sqlx.DB) error {
 
 	retry := func(err error) error {
 		fmt.Printf("%#v: %s\n", err, err)
-		time.Sleep(250 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 		return db.PingContext(ctx)
 	}
 
 	for err != nil {
-		fmt.Printf("%#v\n", db.Stats())
+		//fmt.Printf("%#v\n", db.Stats())
 
 		// Connections to a PG database fail with "connection reset by peer" or
 		// "EOF" until the database has started to respond, then connections will
